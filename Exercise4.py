11a46e092cc46f45dce0d4ce15fb0d0e12ea78db
fedececere <federica.cecere@studio.unibo.it>
2022-03-24 09:16:10 +0100

    exercise 4

---
+++ Exercise4.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 14:44:47 2022
+
+@author: chiaramarzi
+"""
+
+# Complete the libraries importation
+
+def my_plot(x, y, color, title, xlabel, ylabel):
+    
+
+# Compliances values from Table III of Albanese et al., 2016
+Cl = 
+Ct = 
+Cb = 
+CA = 
+Ccw = 
+# Unstressed volumes from Table III of Albanese et al., 2016
+Vul = 
+Vut = 
+Vub = 
+VuA = 
+# Resistances values from Table III of Albanese et al., 2016
+Rml = 
+Rlt = 
+Rtb = 
+RbA = 
+dt = 
+
+# Simulation of 20 seconds
+
+# State variables
+P1 = 
+P2 = 
+P3 = 
+P4 = 
+P5 = 
+
+# Auxiliary variables
+Pl = 
+Ppl = 
+Pt = 
+Pb = 
+PA = 
+
+# Volumes
+Vl = 
+Vt = 
+Vb = 
+VA = 
+VD = 
+
+# Natural breath definition
+T = 5 # period of the natural breath
+
+# Initial conditions
+
+for j in range(#TO COMPLETE):
+               
+# Pressures plots
+
+# Volumes plots
+
+
+# Alveolar ventilation  (at equilibrium)
+Vent = 
+print("Ventiliation:", Vent)
